5a9b94e74604dcd2e3d0ef36ea15e9b5b2d465eb
NotSakura <sakura.noskor@mail.utoronto.ca>
2023-05-27 14:34:49 -0400

    all tasks completed

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -73,7 +73,7 @@ public class Silly implements Comparable<Silly>{
     }
 
     /**
-     * TODO (Task 1): Create another constructor that takes in TWO parameters,
+     * Done (Task 1): Create another constructor that takes in TWO parameters,
      *       both of which are strings.
      *       Afterwards, set this.name to the concatenation of both
      *       strings (e.g. this.name = [first string] + [second string]).
@@ -116,7 +116,7 @@ public class Silly implements Comparable<Silly>{
         Silly y = new Silly("something else");
 
         /**
-         * TODO (Task 2): Below are a bunch of calls to countStatic using
+         * Done (Task 2): Below are a bunch of calls to countStatic using
          *                two different instances of Silly.
          *                Fill out the expected_values array with the
          *                values that countStatic will return at each call.
@@ -142,7 +142,7 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public String toString(){
-        // TODO (Task 3): Implement the body of this method!
+        // Done (Task 3): Implement the body of this method!
         return this.name;
     }
 
@@ -159,7 +159,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public boolean equals(Object o) {
         /**
-         * TODO (Task 4): Complete the body of this method!
+         * Done (Task 4): Complete the body of this method!
          *                We've started it by checking the type of o for you.
          *                You just need to return true if the names are equal.
          */
@@ -200,7 +200,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public int compareTo(Silly other) {
         /**
-         * TODO (Task 5): Implement the body of this method.
+         * Done (Task 5): Implement the body of this method.
          *                A positive number should be returned if this.name
          *                is longer than other.name, a negative number if
          *                other.name is longer than this.name, and 0 if
@@ -221,7 +221,7 @@ public class Silly implements Comparable<Silly>{
     }
 
     /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
+     * Done (Task 6): Submit the changes you made on GitHub!
      *                When you submit it, go to the 'Actions' tab. You should
      *                be able to see this exercise being autograded!
      *
